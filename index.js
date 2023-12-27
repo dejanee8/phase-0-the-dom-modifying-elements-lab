558b3ec8c6b9d1f6f6bcd1519d292d9f0f456b67
Dejanee Gomez <dejaneegomez@gmail.com>
2023-12-27 12:16:05 -0500

    adds elementlab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'This is a new header';
+newHeader.textContent = "Dejanee is the champion";
+newHeader.setAttribute('id', 'victory');
